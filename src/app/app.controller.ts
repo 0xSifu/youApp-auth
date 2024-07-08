e159a809141d3f6ee544321524646300c65d0a26
0xSifu <aldinugroholutfi@gmail.com>
2024-07-08 10:11:26 +0700

    YTT-12 [Service][User] Create Profile

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, VERSION_NEUTRAL } from '@nestjs/common';
+import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import { PrismaService } from 'src/common/services/prisma.service';
+import { Public } from 'src/decorators/public.decorator';
 
-@Controller()
+@Controller({
+  version: VERSION_NEUTRAL,
+  path: '/',
+})
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly healthCheckService: HealthCheckService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/health')
+  @HealthCheck()
+  @Public()
+  public async getHealth() {
+    return this.healthCheckService.check([
+      () => this.prismaService.isHealthy(),
+    ]);
   }
 }
